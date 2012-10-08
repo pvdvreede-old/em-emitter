598edf4548398636bb63209acd35d505c016af40
Paul Van de Vreede <paul@vdvreede.net>
2012-10-08 06:20:24 -0400

    Add cloning of observer data option.

--- lib/em-emitter/observable.rb
+++ lib/em-emitter/observable.rb
@@ -1,7 +1,17 @@
 module EM
   module Emitter
     module Observable
+
+      def uses_clone(cloner)
+        @uses_clone = cloner
+      end
+
       def emit(event, object)
+        # check and see if the observer uses cloning
+        if @uses_clone
+          object = clone_data(object)
+        end
+
         EM::Emitter.emit(self, event, object)
         self
       end
@@ -9,6 +19,21 @@ module EM
       def receiver(event, method_name)
         EM::Emitter.add_observer(self, event, method_name)
         self
+      end
+
+      def clone_data(object)
+        # clone or copy depending on type
+        if object.nil?
+          clone_obj = nil
+        elsif object.respond_to?(:clone)
+          clone_obj = object.clone
+        elsif object.respond_to?(:dup)
+          clone_obj = object.dup
+        else
+          clone_obj = object
+        end
+
+        clone_obj
       end
     end
   end
--- test/emitter_test.rb
+++ test/emitter_test.rb
@@ -60,4 +60,38 @@ class EmitterTest < Test::Unit::TestCase
     end
   end
 
+  def test_cloning_object_option_on
+    @observable.uses_clone true
+    EM.run do
+      hash = { :hash => "value" }
+      assert_not_equal hash, @observable.result, "The observable result is set before testing."
+      assert_not_equal hash.object_id, @observable.result.object_id, "The objects are already the same!"
+
+      EM.add_timer(1) do
+        assert_equal hash, @observable.result, "The observable result was not set."
+        assert_not_equal hash.object_id, @observable.result.object_id, "The objects are not the same!"
+        EM.stop
+      end
+
+      @observable.emit({ :name => "event1" }, hash)
+    end
+  end
+
+  def test_cloning_object_option_off
+      @observable.uses_clone false
+      EM.run do
+        hash = { :hash => "value" }
+        assert_not_equal hash, @observable.result, "The observable result is set before testing."
+        assert_not_equal hash.object_id, @observable.result.object_id, "The objects are already the same!"
+
+        EM.add_timer(1) do
+          assert_equal hash, @observable.result, { :hash => "value" }
+          assert_equal hash.object_id, @observable.result.object_id, "The objects are not the same!"
+          EM.stop
+        end
+
+      @observable.emit({ :name => "event1" }, hash)
+    end
+  end
+
 end
